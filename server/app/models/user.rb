ab13a1152bc09dae9846be3925d706fe8320b1ef
Pedro Alvarado <pedro.alvarado@turner.com>
2017-10-07 06:19:51 -0500

    Add user model and specs close #1

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
 end
--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Attributes" do
+    it { is_expected.to have_attribute :email }
+    it { is_expected.to have_attribute :username }
+    it { is_expected.to have_attribute :photo_url }
+    it { is_expected.to have_attribute :password_digest }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_uniqueness_of :email }
+    it { is_expected.to validate_uniqueness_of :username }
+    it { is_expected.to validate_uniqueness_of :password_digest }
+  end
+
+  describe "Relations" do
+    pending "are missing, add examples!"
+  end
 end
