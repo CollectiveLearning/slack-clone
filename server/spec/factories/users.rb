7e99ebd4e5da1de16adadc4f18035f2cef8463b4
Pedro Alvarado <pedro.alvarado@turner.com>
2017-10-07 06:31:52 -0500

    fancy user factory

--- server/spec/factories/users.rb
+++ server/spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    username "MyString"
-    email "MyString"
-    photo_url "MyString"
-    password_digest "MyString"
+    username "Pedro Alvarado"
+    sequence(:email_address) { |n| "test-#{n}@test.com" }
+    photo_url "http://lorempixel.com/400/200/cats/"
+    password_digest "mUc3m00RsqyRe"
   end
 end
